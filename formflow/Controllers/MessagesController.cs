ebad05eeac736db78ab70faeb804d4e4a5170cfe
James Mann <james.mann@bcs.org>
2016-12-20 08:03:20 +0000

    Add project files.

---
+++ formflow/Controllers/MessagesController.cs
@@ -0,0 +1,28 @@
+﻿using formflow.FormFlow;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using Microsoft.Bot.Connector;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace formflow
+{
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        [ResponseType(typeof(void))]
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity != null && activity.GetActivityType() == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => { return Chain.From(() => FormDialog.FromForm(Enquiry.BuildEnquiryForm)); });
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+    }
+}
---
+++ formflow/FormFlow/Enquiry.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Bot.Builder.FormFlow;
+using System;
+
+namespace formflow.FormFlow
+{
+    [Serializable]
+    public class Enquiry
+    {
+        [Prompt("What's your name?")]
+        public string Name { get; set; }
+        [Prompt("Can I ask which Company you work for?")]
+        public string Company { get; set; }
+        [Prompt("What is your Job Title there?")]
+        public string JobTitle { get; set; }
+        [Prompt("What's the best number to contact you on?")]
+        public string Phone { get; set; }
+        [Prompt("Can you think of anything else that would help us understand your needs?")]
+        public string HowCanWeHelp { get; set; }
+        [Prompt("We send out e-mail correspondence occasionally.  Would you like to receive offers and promotions from us? {||}")]
+        public bool SignMeUpToTheMailingList { get; set; }
+        [Prompt("Do you know which service you require from us? {||}")]
+
+        public Service ServiceRequired { get; set; }
+
+        public enum Service
+        {
+            Consultancy, Support, ProjectDelivery, Unknown
+        }
+
+        public static IForm<Enquiry> BuildEnquiryForm()
+        {
+            return new FormBuilder<Enquiry>()
+                .Field("SignMeUpToTheMailingList")
+                .Field("ServiceRequired")
+                .AddRemainingFields()
+                .Build();
+        }
+    }
+}
